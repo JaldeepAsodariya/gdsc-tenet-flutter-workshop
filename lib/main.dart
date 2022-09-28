728aecf9070f7c72ba0da19389fb239412a12b01
Jaldeep Asodariya <jdpatel.android@gmail.com>
2022-09-28 13:51:42 +0530

    Make calculator functional

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int firstVal = 0;
+  int secondVal = 0;
+  String operation = '';
+  String displayText = '';
+  String result = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,64 +50,146 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.all(16.0),
               alignment: Alignment.bottomRight,
               child: Text(
-                '0',
-                style: Theme.of(context).textTheme.headline4,
+                displayText,
+                style: Theme.of(context).textTheme.headlineLarge,
               ),
             ),
           ),
           Row(
             children: [
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('9'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('8'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('7'),),),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('+'),),),
-              const SizedBox(width: 8.0,),
+              CustomOutlinedButton(
+                label: '9',
+                onPressedButton: () => onButtonClicked('9'),
+              ),
+              CustomOutlinedButton(
+                label: '8',
+                onPressedButton: () => onButtonClicked('8'),
+              ),
+              CustomOutlinedButton(
+                label: '7',
+                onPressedButton: () => onButtonClicked('7'),
+              ),
+              CustomOutlinedButton(
+                label: '+',
+                onPressedButton: () => onButtonClicked('+'),
+              ),
             ],
           ),
           Row(
             children: [
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('6'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('5'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('4'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('-'),)),
-              const SizedBox(width: 8.0,),
+              CustomOutlinedButton(
+                label: '6',
+                onPressedButton: () => onButtonClicked('6'),
+              ),
+              CustomOutlinedButton(
+                label: '5',
+                onPressedButton: () => onButtonClicked('5'),
+              ),
+              CustomOutlinedButton(
+                label: '4',
+                onPressedButton: () => onButtonClicked('4'),
+              ),
+              CustomOutlinedButton(
+                label: '-',
+                onPressedButton: () => onButtonClicked('-'),
+              ),
             ],
           ),
           Row(
             children: [
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('3'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('2'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('1'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('x'),)),
-              const SizedBox(width: 8.0,),
+              CustomOutlinedButton(
+                label: '3',
+                onPressedButton: () => onButtonClicked('3'),
+              ),
+              CustomOutlinedButton(
+                label: '2',
+                onPressedButton: () => onButtonClicked('2'),
+              ),
+              CustomOutlinedButton(
+                label: '1',
+                onPressedButton: () => onButtonClicked('1'),
+              ),
+              CustomOutlinedButton(
+                label: '*',
+                onPressedButton: () => onButtonClicked('*'),
+              ),
             ],
           ),
           Row(
             children: [
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('C'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('0'),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('='),)),
-              const SizedBox(width: 8.0,),
-              Expanded(child: OutlinedButton(onPressed: (){}, child: const Text('/'),)),
-              const SizedBox(width: 8.0,),
+              CustomOutlinedButton(
+                label: 'C',
+                onPressedButton: () => onButtonClicked('C'),
+              ),
+              CustomOutlinedButton(
+                label: '0',
+                onPressedButton: () => onButtonClicked('0'),
+              ),
+              CustomOutlinedButton(
+                label: '=',
+                onPressedButton: () => onButtonClicked('='),
+              ),
+              CustomOutlinedButton(
+                label: '/',
+                onPressedButton: () => onButtonClicked('/'),
+              ),
             ],
           ),
         ],
+      ),
+    );
+  }
+
+  void onButtonClicked(String value) {
+    if (value == 'C') {
+      result = '';
+      displayText = '';
+      firstVal = 0;
+      secondVal = 0;
+    } else if (value == '+' || value == '-' || value == '*' || value == '/') {
+      firstVal = int.parse(displayText);
+      result = '';
+      operation = value;
+    } else if (value == "=") {
+      secondVal = int.parse(displayText);
+      if (operation == '+') {
+        result = (firstVal + secondVal).toString();
+      }
+      if (operation == '-') {
+        result = (firstVal - secondVal).toString();
+      }
+      if (operation == '*') {
+        result = (firstVal * secondVal).toString();
+      }
+      if (operation == '/') {
+        result = (firstVal ~/ secondVal).toString();
+      }
+    } else {
+      result = (displayText + value).toString();
+    }
+    setState(() {
+      displayText = result;
+    });
+  }
+}
+
+class CustomOutlinedButton extends StatelessWidget {
+  const CustomOutlinedButton(
+      {Key? key, required this.label, required this.onPressedButton})
+      : super(key: key);
+
+  final String label;
+  final VoidCallback? onPressedButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        child: OutlinedButton(
+          onPressed: onPressedButton,
+          child: Text(label),
+        ),
       ),
     );
   }
